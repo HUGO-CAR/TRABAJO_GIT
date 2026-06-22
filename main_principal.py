95b7852ba54da56507564a35fe0c4cd82888b46c
Mabry-hub <mau.mardones@duocuc.cl>
2026-06-21 22:11:40 -0400

    Update main_principal.py

--- main_principal.py
+++ main_principal.py
@@ -69,7 +69,96 @@ def leer_opcion():
         print('ERROR: Debe ingresar un número entero.')
         return 0
     
+# Opcion 1
+def registrar_reserva(reservas):
+    print('\n--- REGISTRAR RESERVA ---')
+    codigo = input("Ingrese código de reserva: ").strip()
+    
+    if not validar_codigo(codigo):
+        print("Error: El código no puede estar vacío.")
+        return
 
+   
+    _, encontrada = buscar_reserva_por_codigo(reservas, codigo)
+    if encontrada is not None:
+        print("Error: El código de la reserva ya existe.")
+        return
+
+    nombre = input("Ingrese nombre del huésped: ").strip()
+    if not validar_nombre(nombre):
+        print("Error: El nombre no puede estar vacío.")
+        return
+    
+    noches = leer_entero_positivo("Ingrese la cantidad de noches de estadía: ")
+    valor_noche = leer_entero_positivo("Ingrese el valor por noche: ")
+    
+    # Cálculos automáticos
+    total = noches * valor_noche
+    categoria = calcular_categoria(total)
+    
+
+    nueva_reserva = {
+        "codigo": codigo,
+        "nombre": nombre,
+        "noches": noches,
+        "valor_noche": valor_noche,
+        "total": total,
+        "categoria": categoria
+    }
+    
+    reservas.append(nueva_reserva)
+    print(f"\n¡Reserva registrada con éxito! Categoría asignada: {categoria}")
+
+# Opcion 2:
+def buscar_reserva(reservas):
+    print('\n--- BUSCAR RESERVA ---')
+    codigo = input("Ingrese el código de la reserva a buscar: ").strip()
+    
+    posicion, reserva = buscar_reserva_por_codigo(reservas, codigo)
+    
+    if reserva is not None:
+        print(f"\nReserva encontrada en la posición del registro: {posicion + 1}")
+        print(f"Código: {reserva['codigo']}")
+        print(f"Huésped: {reserva['nombre']}")
+        print(f"Noches: {reserva['noches']}")
+        print(f"Valor por noche: ${reserva['valor_noche']:,}")
+        print(f"Total: ${reserva['total']:,}")
+        print(f"Categoría: {reserva['categoria']}")
+    else:
+        print("Error: No se encontró ninguna reserva con ese código.")
+
+# Opcion 3:
+def actualizar_reserva(reservas):
+    print('\n--- ACTUALIZAR RESERVA ---')
+    codigo = input("Ingrese el código de la reserva a modificar: ").strip()
+    
+    _, reserva = buscar_reserva_por_codigo(reservas, codigo)
+    
+    if reserva is not None:
+        print("\nDeje en blanco si no desea modificar el campo.")
+        
+        
+        nuevo_nombre = input(f"Nombre actual ({reserva['nombre']}): ").strip()
+        if nuevo_nombre != "":
+            reserva['nombre'] = nuevo_nombre
+            
+        
+        modificar_noches = input(f"Noches actuales ({reserva['noches']}) ¿Desea cambiar? (s/n): ").strip().lower()
+        if modificar_noches == 's':
+            reserva['noches'] = leer_entero_positivo("Ingrese nueva cantidad de noches: ")
+            
+        
+        modificar_valor = input(f"Valor noche actual (${reserva['valor_noche']:,}) ¿Desea cambiar? (s/n): ").strip().lower()
+        if modificar_valor == 's':
+            reserva['valor_noche'] = leer_entero_positivo("Ingrese nuevo valor por noche: ")
+            
+        
+        reserva['total'] = reserva['noches'] * reserva['valor_noche']
+        reserva['categoria'] = calcular_categoria(reserva['total'])
+        
+        print("\n¡Reserva actualizada con éxito!")
+    else:
+        print("Error: No se encontró ninguna reserva con ese código.")
 
 
 def eliminar_reserva(reservas):
